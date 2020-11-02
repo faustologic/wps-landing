2334774a6f05a6a0d1db4603824e8d1f62b2ac0a
Miguel Alvarez <ing.migueljalvarez@gmail.com>
2020-11-02 12:06:30 -0500

    add input email validation

--- src/components/Newsletter/newsletter.jsx
+++ src/components/Newsletter/newsletter.jsx
@@ -1,52 +1,81 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { apiUrl } from "../../config/app";
+import { useToasts } from "react-toast-notifications";
 import "./newsletter.css";
 
-class Newsletter extends Component {
-  state = {
-    email: "",
-  };
+const Newsletter = () => {
+  const addToast = useToasts().addToast;
+  const [email, setEmail] = useState("");
+  let error = "";
+  const handleValidation = () => {
+    let formIsValid = true;
+    if (email.length === 0) {
+      formIsValid = false;
+      error = "el email es requerido";
+    }
+    if (typeof email !== undefined) {
+      let lastAtPos = email.lastIndexOf("@");
+      let lastDotPos = email.lastIndexOf(".");
 
-  handlerSendEmail = () => {
-    console.log("Email Send!");
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-      },
-      body: JSON.stringify({ email: this.state.email }),
+      if (
+        !(
+          lastAtPos < lastDotPos &&
+          lastAtPos > 0 &&
+          email.indexOf("@@") === -1 &&
+          lastDotPos > 2 &&
+          email.length - lastDotPos > 2
+        )
+      ) {
+        formIsValid = false;
+        error = "el email no es valido.";
+      }
+    }
+    return formIsValid;
+  };
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    if (!handleValidation()) {
+      return addToast(error, { appearance: "error", autoDismiss: true });
+    }
+    const data = {
+      email: email,
     };
-    fetch(
-      "http://localhost:3001/addNewSuscriber",
-      requestOptions
-    ).then((response) => console.log(response.json()));
-  };
 
-  setEmail = (param) => {
-    this.setState({
-      email: param,
-    });
+    return axios
+      .post(`${apiUrl}/subcriptions`, data, {
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      })
+      .then((response) => {
+        addToast(`${response.data.email} ha sido registrado exitosamente`, {
+          appearance: "success",
+          autoDismiss: true,
+        });
+      })
+      .catch((err) => {
+        if (err.response.data.message.includes('unique')) {
+          addToast('el email ya existe en nuestra base de datos', {
+            appearance: "error",
+            autoDismiss: true,
+          });
+        }
+      });
   };
-
-  render() {
-    return (
-      <div className="newsletter">
-        <div className="newsletter__form">
-          <input
-            className="newsletter__input"
-            text="email"
-            value={this.state.email}
-            onChange={(e) => this.setEmail(e.target.value)}
-            placeholder="Ingresa tu correo"
-            required
-          />
-          <button className="newsletter__btn" onClick={this.handlerSendEmail}>
-            AVÍSAME
-          </button>
-        </div>
-      </div>
-    );
-  }
-}
+  return (
+    <form className="newsletter__form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        text="email"
+        className="newsletter__input"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input type="submit" className="newsletter__btn" value="AVISAME" />
+    </form>
+  );
+};
 
 export default Newsletter;
---
+++ src/config/app.js
@@ -0,0 +1,2 @@
+const { REACT_APP_API_URL } = process.env;
+export const apiUrl = REACT_APP_API_URL;
