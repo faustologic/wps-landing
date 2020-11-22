aed8b84df2595934eebaed83da3b58d58a5271ff
Miguel Alvarez <ing.migueljalvarez@gmail.com>
2020-11-21 19:18:35 -0500

    change toast for po-pup

--- src/components/Content/content.jsx
+++ src/components/Content/content.jsx
@@ -3,7 +3,6 @@ import "./contentUpdate.css";
 import logo from "../../assets/logo.png";
 import Newsletter from "../Newsletter/newsletter";
 import SocialIcons from "../socialIcons/socialIcons";
-import { ToastProvider } from "react-toast-notifications";
 
 class Content extends Component {
   render() {
@@ -14,9 +13,7 @@ class Content extends Component {
             <img className="content__img" src={logo} alt="" />
           </div>
           <div className="content__newsletter">
-            <ToastProvider>
-              <Newsletter />
-            </ToastProvider>
+          <Newsletter />
             <p>
               Regístrate usando tu correo electrónico o conéctate a nuestras
               redes sociales para ser el primero en enterarte.
--- src/components/Newsletter/newsletter.jsx
+++ src/components/Newsletter/newsletter.jsx
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { apiUrl } from "../../config/app";
-import { useToasts } from "react-toast-notifications";
+import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from "reactstrap";
+
+
 import "./newsletter.css";
 
 const Newsletter = () => {
-  const addToast = useToasts().addToast;
   const [email, setEmail] = useState("");
-  
+  const [modal, setModal] = useState({ open: false });
+  const [message, setMessage] = useState("");
+  const modalStyles = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  };
   let error = "";
   const handleValidation = () => {
     let formIsValid = true;
@@ -34,10 +42,16 @@ const Newsletter = () => {
     }
     return formIsValid;
   };
+  const openModal = () => {
+    return setModal({
+      open: !modal.open,
+    });
+  };
   const handleSubmit = (evt) => {
     evt.preventDefault();
     if (!handleValidation()) {
-      addToast(error, { appearance: "error", autoDismiss: true });
+      setMessage(error);
+      openModal();
       return setEmail("");
     }
     const data = {
@@ -52,36 +66,51 @@ const Newsletter = () => {
         },
       })
       .then((response) => {
-        addToast(
-          `¡${response.data.email} ha sido registrado de manera exitosa!`,
-          {
-            appearance: "success",
-            autoDismiss: true,
-          }
+        setMessage(
+          `¡${response.data.email} ha sido registrado de manera exitosa!`
         );
+        openModal();
         setEmail("");
       })
       .catch((err) => {
         if (err.response.data.message.includes("unique")) {
-          addToast("¡Este email ya existe en nuestra base de datos!", {
-            appearance: "error",
-            autoDismiss: true,
-          });
+          setMessage("¡Este email ya existe en nuestra base de datos!");
+          openModal();
           setEmail("");
         }
       });
   };
+
   return (
-    <div className="newsletter__form">
-      <input
-        type="text"
-        text="email"
-        className="newsletter__input"
-        value={email}
-        placeholder="Ingrese su email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input type="submit" className="newsletter__btn" value="AVISAME" onClick={handleSubmit} />
+    <div>
+      <div className="newsletter__modal">
+        <Modal isOpen={modal.open} style={modalStyles}>
+          <ModalHeader>Notificacíon</ModalHeader>
+          <ModalBody>{message}</ModalBody>
+          <ModalFooter>
+            <Button className="newsletter__btn_close" onClick={openModal}>
+              Cerrar
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+
+      <div className="newsletter__form">
+        <input
+          type="text"
+          text="email"
+          className="newsletter__input"
+          value={email}
+          placeholder="Ingrese su email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="submit"
+          className="newsletter__btn"
+          value="AVISAME"
+          onClick={handleSubmit}
+        />
+      </div>
     </div>
   );
 };
